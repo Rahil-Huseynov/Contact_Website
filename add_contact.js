e0ed221ff3951a31629f7b75d9c7da6855f13de1
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2024-05-23 19:46:35 +0400

    fix bug RegExp

--- add_contact.js
+++ add_contact.js
@@ -1,76 +1,84 @@
 const add_contact_button = document.getElementById('add_contact_button');
 
-const contact_menu = document.getElementById('contact_menu')
+const contact_menu = document.getElementById('contact_menu');
 
-contact_menu.addEventListener('click', () =>{
+const error = document.getElementById('error');
+
+contact_menu.addEventListener('click', () => {
+    
     window.location.href = "index.html";
-})
+
+});
+
 add_contact_button.addEventListener('click', () => {
-
+    
     const name = document.getElementById('name');
-    
+
     const surname = document.getElementById('surname');
-    
+
     const email = document.getElementById('email');
-    
+
     const number = document.getElementById('number');
-    
+
     const description = document.getElementById('description');
-    
-    const emailCheck = /^([\w\.\_\!]{1,10})\@([a-z0-9]{2,10})\.([a-z]{2,3})$/
 
-    const nameCheck = /^([A-Za-z]{2,10)$/
+    const emailCheck = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const surnameCheck = /^([A-Za-z]{2,10)$/
-
-    const numberCheck = /^(\+\d{1,3}[- ]?)?\d{10}$/
+    const nameCheck = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńñòóôöõøśšùúûüųūÿýżźžÀÁÂÄÃÅĄČĆĘÈÉÊËĖĮÌÍÎÏŁŃÑÒÓÔÖÕØŚŠÙÚÛÜŲŪŸÝŻŹŽ\-'\s]{1,50}$/;
     
+    const surnameCheck = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńñòóôöõøśšùúûüųūÿýżźžÀÁÂÄÃÅĄČĆĘÈÉÊËĖĮÌÍÎÏŁŃÑÒÓÔÖÕØŚŠÙÚÛÜŲŪŸÝŻŹŽ\-'\s]{1,50}$/;
     
+    const numberCheck = /^(\+\d{1,3}[- ]?)?\d{10}$/;
 
     if (emailCheck.test(email.value)) {
-        email.style.bordercolor = 'green';
-    }
-    else{
+        email.style.borderColor = 'green';
+    } else {
         email.style.borderColor = 'red';
     }
-
     if (nameCheck.test(name.value)) {
-        name.style.bordercolor = 'green';
-    }
-    else{
+        name.style.borderColor = 'green';   
+    } else {
         name.style.borderColor = 'red';
     }
-
     if (surnameCheck.test(surname.value)) {
-        surname.style.bordercolor = 'green';
-    }
-    else{
+        surname.style.borderColor = 'green';
+    } else {
         surname.style.borderColor = 'red';
     }
-
     if (numberCheck.test(number.value)) {
-        number.style.bordercolor = 'green';
-    }
-    else{
+        number.style.borderColor = 'green';
+    } else {
         number.style.borderColor = 'red';
     }
 
-    const contact = {
-    
-        name: name.value,
-    
-        surname: surname.value,
-    
-        email: email.value,
-    
-        number: number.value,
-    
-        description: description.value
-    
-    };
+    const isNameValid = nameCheck.test(name.value);
 
-    console.log(contact);
+    const isSurnameValid = surnameCheck.test(surname.value);
+    
+    const isEmailValid = emailCheck.test(email.value);
+    
+    const isNumberValid = numberCheck.test(number.value);
 
-    localStorage.setItem('contacts', JSON.stringify(contact));
+    if (isEmailValid && isNameValid && isSurnameValid && isNumberValid) {
+        const contact = {
+            name: name.value,
 
+            surname: surname.value,
+
+            email: email.value,
+
+            number: number.value,
+
+            description: description.value
+        };
+        localStorage.setItem('contacts', JSON.stringify(contact));
+
+        error.innerHTML = `Information added successfully`;
+
+        error.style.color = 'green';
+    } else {
+        error.innerHTML = `The data was entered incorrectly`;
+
+        error.style.color = 'red';
+    }
 });
