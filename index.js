56db89d02d66ff8ba7f356681d7609d9755f0bd6
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2024-05-26 01:33:11 +0400

    Improvement works

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const add_contact_button = document.getElementById('add_contact_button');
 
+const search_button = document.getElementById('search_button');
+
+const search_input = document.getElementById('search_input');
+
 const add_contact = document.getElementById('add_contact');
 
 let all_contact = [];
@@ -20,43 +24,114 @@ document.addEventListener('DOMContentLoaded', () => {
 
         all_contact = contacts;
 
-        contacts.forEach(contact => {
-
-            const contactDiv = document.createElement('div');
-
-            const name = document.createElement('p');
-
-            name.innerHTML = contact.name;
-
-            const surname = document.createElement('p');
-
-            surname.innerHTML = contact.surname;
-
-            const email = document.createElement('p');
-
-            email.innerHTML = contact.email;
-
-            const number = document.createElement('p');
-
-            number.innerHTML = contact.number;
-
-            const description = document.createElement('p');
-
-            description.innerHTML = contact.description;
-
-            contactDiv.appendChild(name);
-
-            contactDiv.appendChild(surname);
-
-            contactDiv.appendChild(email);
-
-            contactDiv.appendChild(number);
-
-            contactDiv.appendChild(description);
-
-            add_contact.appendChild(contactDiv);
-        });
+        displayContacts(contacts);
 
         console.log(all_contact);
     }
+});
+
+
+function displayContacts(contacts) {
+
+    add_contact.innerHTML = ''; 
+
+    contacts.forEach((contact, index) => {
+        const container = document.createElement('div');
+
+        const contactDiv = document.createElement('div');
+
+        const imgcontainer = document.createElement('div');
+
+        imgcontainer.classList.add('imgcontainer');
+
+        const img = document.createElement('img');
+
+        img.classList.add('img');
+
+        img.src = './image/people.png';
+
+        contactDiv.classList.add('contactDiv');
+
+        const name = document.createElement('p');
+
+        name.innerHTML = contact.name;
+
+        const surname = document.createElement('p');
+
+        surname.innerHTML = contact.surname;
+
+        const email = document.createElement('p');
+
+        email.innerHTML = contact.email;
+
+        const number = document.createElement('p');
+
+        number.innerHTML = contact.number;
+
+        const down = document.createElement('img');
+
+        down.classList.add('down');
+
+        down.src = './image/down-arrow.png';
+
+        const del = document.createElement('img');
+
+        del.classList.add('down');
+
+        del.src = './image/delete.png';
+
+        const description = document.createElement('p');
+        
+        description.innerHTML = contact.description;
+        
+        description.classList.add('description');
+
+        imgcontainer.appendChild(img);
+        
+        contactDiv.appendChild(imgcontainer);
+        
+        contactDiv.appendChild(name);
+        
+        contactDiv.appendChild(surname);
+        
+        contactDiv.appendChild(email);
+        
+        contactDiv.appendChild(number);
+        
+        contactDiv.appendChild(del);
+        
+        contactDiv.appendChild(down);
+        
+        contactDiv.appendChild(description);
+        
+        container.appendChild(contactDiv);
+        
+        add_contact.appendChild(container);
+
+        down.addEventListener('click', () => {
+            contactDiv.style.height = '200px';
+        });
+
+        del.addEventListener('click', () => {
+            all_contact.splice(index, 1); 
+        
+            localStorage.setItem('contacts', JSON.stringify(all_contact)); 
+        
+            displayContacts(all_contact); 
+        });
+    });
+}
+
+search_button.addEventListener('click', () => {
+
+    const searchValue = search_input.value.toLowerCase();
+
+    const filteredContacts = all_contact.filter(contact =>
+
+        contact.name.toLowerCase().includes(searchValue)
+
+    );
+
+    displayContacts(filteredContacts);
+
 });
